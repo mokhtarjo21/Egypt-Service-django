d2c72efb9fbfb8bbb79a4922083ec97165ca489e
mokhtarjo21 <mokhtar.jo21@gmail.com>
2025-06-14 19:32:40 +0300

    remove import openai

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ from django.utils.decorators import method_decorator
 from .serializers import *
 from django.http import JsonResponse
 import json
-from openai import OpenAI ,RateLimitError
+
 from rest_framework.decorators import api_view , permission_classes
 from rest_framework.views import APIView
 from rest_framework.response import Response
